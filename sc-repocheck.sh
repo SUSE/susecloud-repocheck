9944b5ba796a7fb434f88228377790f5e592105c
rfparedes <rparedes@gmail.com>
2020-11-07 14:22:26 -0500

    Added additional options to strace of registercloudguest

--- sc-repocheck.sh
+++ sc-repocheck.sh
@@ -319,7 +319,7 @@ function check_region_servers() {
 #   tmp_dir 
 #######################################
 function registercloudguestnow() {
-  strace -f -s 128 -o $1/strace.out /usr/sbin/registercloudguest --force-new 
+  strace -fttTyy -s 256 -o $1/strace.out /usr/sbin/registercloudguest --force-new 
 }  
 #######################################
 # End of run report
