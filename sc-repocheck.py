cbf455bf9946f6f17fcaa868801e51e37212d645
Rich Paredes <rich.paredes@suse.com>
2021-02-17 17:48:01 -0500

    Change strace to -s512

--- sc-repocheck.py
+++ sc-repocheck.py
@@ -2375,7 +2375,7 @@ def collect_debug_data(framework, disable_tcpdump, disable_metadata_collect):
     strace_file = os.path.join(tmp_dir, "strace.out")
 
     try:
-        subprocess.call(['strace', '-f', '-o', strace_file, '/usr/sbin/registercloudguest', '--force-new'])
+        subprocess.call(['strace', '-f', '-s512', '-o', strace_file, '/usr/sbin/registercloudguest', '--force-new'])
     except:
         logging.error("PROBLEM: Cannot run registercloudguest. There are unknown issues. Please provide debug data.")
 
